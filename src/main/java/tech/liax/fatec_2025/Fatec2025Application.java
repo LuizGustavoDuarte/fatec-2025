9bc45b5600c86dd8ddae20ad6f260debffef5f3f
Adson Vitorio <adsonluis81@gmail.com>
2025-10-04 11:58:29 -0300

    Initializing project

---
+++ src/main/java/tech/liax/fatec_2025/Fatec2025Application.java
@@ -0,0 +1,13 @@
+package tech.liax.fatec_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fatec2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fatec2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/tech/liax/fatec_2025/Fatec2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.liax.fatec_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fatec2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
